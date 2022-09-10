303bc19a9fd389d94c25640f8d6243edf2e66850
finalsurgery <alex.duval.cid@outlook.com>
2022-09-10 12:31:02 -0400

    multi file parsing and organization

--- main.py
+++ main.py
@@ -2,20 +2,19 @@
 
 import os
 
-
-
-home_path = "/home/aduvalcid/" # user's home path, the script assume that your Download folder is in this path,
-destination_path = "/home/aduvalcid/" # in my case this path is the same but you can put a different path if you want the folders and files to end up somewhere else.
-affected_folder = "Downloads/" # the folder you want to sort through.
-check_path = [f"{home_path}{affected_folder}"] # combined output that defines the folder that will be sorted through.
+home_path = "C:/Users/alexd/" # user's home path, the script assume that your Download folder is in this path,
+destination_path = "C:/Users/alexd/Documents" # in my case this path is the same but you can put a different path if you want the folders and files to end up somewhere else.
+affected_folder = ["Downloads/", "Pictures/", "Music/", "Documents/", "Videos/"] # the folder you want to sort through.
+ # combined output that defines the folder that will be sorted through.
 
 # below are key value pairs, the extensions of the files you want to move are the values, 
 # the directory you want it to end up in is a combination of "home_path" variable and the key
 formats = {
-    "Pictures/" : ("jpeg", "jpg", "png", "gif"),
-    "Documents/" : ("pdf", "doc", "docx"),
-    "Archive/" : ("rar", "zip"),
-    "delete/" : ("torrent")
+    "/Pictures/" : ("jpeg", "jpg", "png", "gif", "bmp"),
+    "/Documents/" : ("pdf", "doc", "docx", "html", "log", "swf", "txt", "odt"),
+    "/Archive/" : ("rar", "zip", "7z", "tar"),
+    "/Application/" : ("exe", "msi"),
+    "/delete/" : ("torrent", "dll", "save", "iso", "yaml", "py", "cfg")
 }
 
 # parses filenames to narrow down the results to an extension.
@@ -31,10 +30,10 @@ def establish_location(ext):
 
 # standard method for cleaning up strings.
 def cleaner(path):
-    special_characters = "\"[]\'"
-    for i in special_characters:
-        path = str(path).replace(i,"")
-    return path 
+    brain_path = [f"{home_path}{path}"]
+    special_characters = "[]\'\""
+    check_path = str(brain_path[0])
+    return check_path
 
 # the primary logic of the script, it will establish if it needs to delete a file or not and move it to it's new location.
 def sort_file_logic(old,new,delete):
@@ -48,26 +47,29 @@ def sort_file_logic(old,new,delete):
 
 # main logic in charge of parsing outputs, error handling and looping the application 
 def main():
-    path = cleaner(check_path)
-    directory_list = os.listdir(path)
-    for file in directory_list:
-        folder = os.path.isdir(file)
-        if folder == False:
-            ext = find_extension(file)
-            try:
-                old_location = f"{path}{file}"
-                new_location = f"{establish_location(ext)}{file}"
+    for folders in affected_folder:
+        path = cleaner(folders)
+        directory_list = os.listdir(path)
+        for file in directory_list:
+            folder = os.path.isdir(file)
+            if folder == False:
+                ext = find_extension(file)
                 try:
-                    sort_file_logic(old=old_location,new=new_location,delete=establish_location(ext))
-                except FileNotFoundError: # if you want to add a new directory and new extensions in the dict above this will create the directory and recursivly run the function to continue the output
-                    if "delete" not in establish_location(ext):
-                        os.mkdir(establish_location(ext))                
-                        main()
-            except IndexError as e:
-                print(f"indexerror: {e}")
-            
+                    old_location = f"{path}{file}"
+                    new_location = f"{establish_location(ext)}{file}"
+                    try:
+                        sort_file_logic(old=old_location,new=new_location,delete=establish_location(ext))
+                    except FileNotFoundError: # if you want to add a new directory and new extensions in the dict above this will create the directory and recursivly run the function to continue the output
+                        if "delete" not in establish_location(ext):
+                            os.mkdir(establish_location(ext))                
+                            main()
+                except IndexError as e:
+                    print(f"indexerror: {e}")
+                except PermissionError as b:
+                    print(f"perission error: {b}")
                 
-        
+                    
+            
 
 if __name__=="__main__":
     main()
